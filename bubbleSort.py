d9baf1771d7b1ae23f370148e587d54322d68301
kannasuresh99 <72544581+kannasuresh99@users.noreply.github.com>
2021-01-08 21:35:44 +0530

    Update bubbleSort.py

--- bubbleSort.py
+++ bubbleSort.py
@@ -1,11 +1,7 @@
-import math
-
 a = [90, -9, -87, 22, 53, 0, 2, -32, 88, 101]
-b = []
-k = 0
 for x in range(0, len(a)):
     for i in range(0, len(a)):
-        if i < (len(a)-2) and a[i] > a[i+1]:
+        if i < (len(a)-1) and a[i] > a[i+1]:
             tempVariable = a[i]
             a[i] = a[i+1]
             a[i+1] = tempVariable
